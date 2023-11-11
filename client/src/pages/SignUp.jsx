a5842ce092b399fcf97dbfb5f1bac85a1ad92dfb
pkkhvu <hungkevinvu@gmail.com>
2023-11-10 18:18:51 -0800

    Completed sign-up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,32 +1,83 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      //target refers to the element that triggered the event (button click, input change, etc.)
+      //e.target.id gives you the element's ID that triggered the event
+      //e.target.value gives you the element's CURRENT value entered or selected in that input that triggered the event
+      //dynamic key, as each input has its own ID, so to access each input's value; you target its key.
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  //form stuff
+  const handleSubmit = async (e) => {
+    //ensures a page doesn't reload like a normal form submission would do
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       {/* flex class, stacking the flex elements with a gap of 4 */}
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70">
-          Sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70"
+        >
+          {loading ? "loading" : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -35,6 +86,8 @@ export default function SignUp() {
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {/* if error is true, red-text-error is displayed. If false, displays nothing */}
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+//This configuration sets up a proxy for requests starting with "/api". Any request that starts with "/api" will be forwarded to the specified target, in this case, "http://localhost:3333". The secure: false option is used when the target is not using HTTPS.
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3333",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
