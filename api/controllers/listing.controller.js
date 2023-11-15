6198f3e9d738269d0d74f41f91fbafdeba46d630
pkkhvu <hungkevinvu@gmail.com>
2023-11-15 15:58:50 -0800

    delete an individual listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -5,6 +5,21 @@ export const create = async (req, res, next) => {
     const listing = await Listing.create(req.body);
     return res.status(201).json({ listing });
   } catch (error) {
+    D;
+    next(error);
+  }
+};
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listings!"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+  } catch (error) {
     next(error);
   }
 };
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { create } from "../controllers/listing.controller";
+import { deleteListing } from "../controllers/listing.controller";
 import { verifyToken } from "../utils/verifyUser";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+router.post("/delete/:id", verifyToken, deleteListing);
 
 export default router;
