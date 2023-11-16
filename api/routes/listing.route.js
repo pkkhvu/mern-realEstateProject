1b3607ef2d3b01d5ddd8d210e0351aa8ba538ac7
pkkhvu <hungkevinvu@gmail.com>
2023-11-15 17:29:12 -0800

    allow a user to update listings

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { create } from "../controllers/listing.controller";
 import { deleteListing } from "../controllers/listing.controller";
+import { updateListing } from "../controllers/listing.controller";
+import { getListing } from "../controllers/listing.controller";
 import { verifyToken } from "../utils/verifyUser";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.post("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", verifyToken, getListing);
 
 export default router;
