e9206ba2290d1a4760d4626755184d76e048eb6f
pkkhvu <hungkevinvu@gmail.com>
2023-11-17 08:27:22 -0800

    Import fix

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,7 +7,7 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
-import { app } from "../firebase.js";
+import app from "../firebase";
 import {
   updateUserStart,
   updateUserSuccess,
